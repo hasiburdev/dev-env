8f828324927d5746b300f7364c4a3530a776b5db
Hasibur Rahman <hasiburdev@gmail.com>
2026-07-19 02:05:55 +0600

    feat: add dev-killport script and update alias configuration

--- config/alias.sh
+++ config/alias.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Application Aliases
 alias cid="code-insiders ."
 alias google-chrome="google-chrome-stable"
+alias zed="zeditor ."
 
-alias dud="docker compose up -d"
+# Docker Aliases
+alias dupd="docker compose up -d"
+alias ddown="docker compose down"
 
+
+# NodeJs Related Aliases
 alias dev="pnpm run dev"
 alias pdev="pnpm run dev"
 
+# Go Related Aliases
 alias task='go-task'
